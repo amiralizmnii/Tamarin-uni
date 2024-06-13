344c76fb07bd21810a3a7fb41e9fa2005010ac4a
amiralizmnii <162716256+amiralizmnii@users.noreply.github.com>
2024-06-13 11:59:03 +0330

    Add files via upload

---
+++ Hasel jam.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+  int a,b;
+  cout<<"adad aval ra vared konid: " ;
+  cin>>a;
+  cout<< "adad dovom ra vared konid: ";
+  cin>>b;
+  cout<<"hasel jam adad aval va adad dovom: "<<a+b;
+return 0;
+}
