703e305a7822568b5bec830a6ad30974b599acfc
amiralizmnii <162716256+amiralizmnii@users.noreply.github.com>
2024-06-13 12:03:12 +0330

    Add files via upload

---
+++ Hasel jam.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std; //dige niaz be std:: ghable har khat nadarim
+int main (){
+  int a,b; //adad hara tarif mikonim
+  cout<<"adad aval ra vared konid: " ; //karbar adad aval ra vared mikonad
+  cin>>a;
+  cout<< "adad dovom ra vared konid: "; //karbar adad dovom ra vared mikonad
+  cin>>b;
+  cout<<"hasel jam adad aval va adad dovom: "<<a+b;
+return 0;
+}
